7d2e20435437b4b5dbe1098f6ea7b3cf6c96ad9f
Aaron Baker <aaronbaker9210@gmail.com>
2022-10-24 11:33:40 -0500

    fix import path casing for deployment

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import Layout from '/components/Layout'
+import Layout from '/components/layout'
 import '/styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
